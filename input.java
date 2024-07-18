f03cc00d6aeac7f4fcbcf844bbbb34e8d0df3cd1
maviramos <vitoriamariaifc@gmail.com>
2024-07-18 10:52:53 -0300

    meu primeiro java

---
+++ input.java
@@ -0,0 +1,31 @@
+//in é a entrada (teclado) 
+// out é a saida 
+// sempre colocar ; no final de cada linha 
+// void significa nada/vazio, não retorna nada 
+// arquivo sempre tem que ser salvo com nome da classe
+//args é argurmentos, (String[]args) vetor de argurmentos 
+// input pode ser substituido por uma variavel, ex: de Scanner input  p/ Scanner s 
+// nextLine pega a linha completa inclusive o enter e o enter pega a linha inteira automáticamente, * 
+// * next vai pegar so o proximo por isso da erro ao colocar so ele
+
+
+
+
+
+import java.util.Scanner;
+import javax.sql.rowset.spi.SyncFactory;
+class input {
+      public static void main(String[]args){
+        Scanner input = new Scanner (System.in);
+        System.out.print("Digite seu nome:");
+        String nome = input.nextLine();
+        System.out.print("Informe sua idade:");
+        int idade = input.nextInt();
+
+
+        System.out.println("Hello," + nome +
+                        " sua idade é:" + idade);
+   input.close();
+      }
+    }
+
